e921bea02fd935088f664d81f0bcaa822de5d327
Giorgi Gonashvili <ggona14@freeuni.edu.ge>
2023-08-29 18:27:08 +0400

    Update telegram client library. Add logging into database

--- main.py
+++ main.py
@@ -1,34 +1,53 @@
-from telefone import Bot
-
-from gensim.models.doc2vec import Doc2Vec
 import pickle
-from nltk.tokenize import word_tokenize
+import telebot
 
+from utils import *
 
-bot = Bot("5375634736:AAEKhrNYg7bpoDJ0ykzOZXtdll52WDKrXLw")
+import nltk
+nltk.download('punkt')
+
+config_data = get_config()
+sql_credentials = config_data['mysql_database']
+bot_credentials = config_data['telegram_bot_token']
+
+bot = telebot.TeleBot(bot_credentials['token'])
+
 
 d = {
     0 : 'Truth',
     1 : 'Fake'
 }
 
-text_vectorizer_model = Doc2Vec.load("trained_models/text_vectorizer.model")
-with open('trained_models/knn.model' , 'rb') as f:
+text_vectorizer_model = Doc2Vec.load("trained_models/text_vectorizer_old.model")
+with open('trained_models/knn_old.model' , 'rb') as f:
     knn = pickle.load(f)
 
-def predict(vectorizer_model, knn_model, msg):
-    tokenized = word_tokenize(msg.lower())
-    vector = vectorizer_model.infer_vector(tokenized)
-    prediction = knn_model.predict([vector])[0]
+@bot.message_handler(func=lambda message: True)
+def handler(msg) -> str:
+    try:
 
-    return d[prediction]
+        message_text = msg.text
+        defective_message_repsponse = check_message_info(message_text)
 
-@bot.on.message()
-async def handler(msg) -> str:
-    message_text = msg.text
-    prediction = predict(text_vectorizer_model, knn, message_text)
-    first_name = msg.from_.first_name
+        if  defective_message_repsponse != "":
+            bot.reply_to(msg, defective_message_repsponse)
+            log_message_into_db(sql_credentials, message_text, defective_message_repsponse, 'Defective')
+            return defective_message_repsponse
 
-    return "Hi, {} This news sounds like {}".format(first_name, prediction)
+        prediction = predict(text_vectorizer_model, knn, d, message_text)
+        first_name = msg.from_user.first_name
+        response_message = "Hi, {} This news sounds like {}".format(first_name, prediction)
 
-bot.run_forever()
+        bot.reply_to(msg, response_message)
+        log_message_into_db(sql_credentials, message_text, response_message, prediction)
+
+        return response_message
+
+    except Exception as e: 
+        print(e)
+        return "Please provide more information."
+
+bot.infinity_polling()
+
+
+
