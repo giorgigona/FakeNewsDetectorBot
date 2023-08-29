1f48e66a9367f026312c2a919a1cc752e2f2f675
Giorgi Gonashvili <ggona14@freeuni.edu.ge>
2023-08-29 18:33:20 +0400

    Add TelegramBotLog.sql

---
+++ TelegramBotLog.sql
@@ -0,0 +1,12 @@
+-- CREATE TABLE TelegramBotLog (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--     message_text TEXT,
+--     response_text TEXT,
+--     prediction varchar(10)
+-- );
+
+
+-- select * from TelegramBotLog order by created_at DESC ;
+
+-- truncate table TelegramBotLog;
